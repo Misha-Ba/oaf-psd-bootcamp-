a72f402a5df80af929270abb0e18b9d68bf40c34
Kamisha <kamishabaijnauth2003@gmail.com>
2024-07-22 00:08:41 -0400

    Created new file with function that determines whether a number is prime or not

---
+++ is_prime.py
@@ -0,0 +1,19 @@
+def is_prime(n):
+    """
+    Check if a number is prime.
+    Returns:
+    bool: True if the number is prime, False otherwise.
+    """
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
